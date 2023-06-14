4cc32f4633c689bbf1c25b299f5929b4026a7f1d
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-14 17:17:07 +0200

    creo componenti

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        message: "Salve"
+    }
+}
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style>
+h1 {
+    font-size: 1.5rem;
+    color: aqua;
+}
+</style>
